75867e23b2264f9a7bf2448f668506cc93dcf439
Ajten <ajtencani@gmail.com>
2022-03-18 13:12:45 +0100

    Show horizontal scrolling onboarding

--- components/Onboarding/Onboarding.js
+++ components/Onboarding/Onboarding.js
@@ -1,23 +1,45 @@
-import React from "react";
-import {View,Text,StyleSheet} from "react-native";
+import React, {useRef, useState} from "react";
+import {Animated, FlatList, StyleSheet, View} from "react-native";
+import OnboardingItem from "./OnboardingItem";
+import data from "../../constants/data";
 
 const Onboarding = () => {
-    return(
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const slidesRef = useRef(null);
+    const viewableItemsChanged = useRef(({viewableItems}) => {
+        setCurrentIndex(viewableItems[0].index);
+    }).current;
+    const viewConfig = useRef({viewAreaCoveragePercentThreshold: 50}).current;
+
+    return (
         <View style={styles.container}>
-            <Text>
-                Onboarding
-            </Text>
+            <View style={{flex: 3}}>
+                <FlatList
+                    data={data}
+                    renderItem={(item) => <OnboardingItem item={item}/>}
+                    horizontal
+                    showsHorizontalScrollIndicator
+                    pagingEnabled
+                    bounces={false}
+                    keyExtractor={(item) => item.id}
+                    onScroll={Animated.event([{nativeEvent: {contentOffset: {x: scrollX}}}], {
+                        useNativeDriver: false,
+                    })}
+                    scrollEventThrottle={32}
+                    onViewableItemsChanged={viewableItemsChanged}
+                    viewabilityConfig={viewConfig}
+                    ref={slidesRef}
+                />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
+    },
 });
 
 export default Onboarding
