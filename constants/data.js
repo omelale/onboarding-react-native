e0b327bebfcbc3e265a3911a98581ec687b31cc9
Ajten <ajtencani@gmail.com>
2022-03-18 11:59:37 +0100

    Added information to display in single onboarding page

---
+++ constants/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id : '1',
+        title: 'Quick and easy Payments',
+        description: 'Grow your business by accepting card payments with the new card reader.',
+        image: require('assets/credit-card.png')
+    },
+    {
+        id : '2',
+        title: 'Smart Point of Sale - POS',
+        description: 'Complete point of sale software tailored to your business needs.',
+        image: require('assets/computer.png')
+    },
+    {
+        id : '3',
+        title: 'Instant Notifications',
+        description: 'Instant notifications let you quickly see new purchases and messages.',
+        image: require('assets/notifications.png')
+    },
+    {
+        id : '4',
+        title: 'Free customization',
+        description: 'Feel free to adjust your system as you wish to speed up your checkout.',
+        image: require('assets/customize.png')
+    }
+]
